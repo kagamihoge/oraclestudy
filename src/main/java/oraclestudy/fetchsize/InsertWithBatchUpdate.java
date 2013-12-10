051a0890cafc316cae9f292b4fc4970ba8c18153
kagamihoge <kagami2spiegel@gmail.com>
2013-12-10 19:45:18 +0900

    JDBCのaddBatchとexecuteBatchの頻度変更時の動きをstatspackで
    http://d.hatena.ne.jp/kagamihoge/20131210/1386672188

---
+++ src/main/java/oraclestudy/fetchsize/InsertWithBatchUpdate.java
@@ -0,0 +1,35 @@
+package oraclestudy.fetchsize;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class InsertWithBatchUpdate {
+
+    public static void main(String[] args) throws Exception {
+        long start = System.currentTimeMillis();
+
+        final String insertStr = "INSERT INTO insert_to_million_rows_table VALUES (?)";
+        try (Connection connection = DriverManager.getConnection(
+            "jdbc:oracle:thin:@192.168.0.20:1521:XE", "kagamihoge", "xxxx");
+            PreparedStatement sql = connection.prepareStatement(insertStr);) {
+            connection.setAutoCommit(false);
+            
+            for (int i=1; i<=1_000_000; i++) {
+                sql.setString(1, RandomStringUtils.randomAlphanumeric(20));
+                sql.addBatch();
+                if (i % 100 == 0) {
+                    sql.executeBatch();
+                }
+            }
+            
+            connection.commit();
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+    }
+
+}
